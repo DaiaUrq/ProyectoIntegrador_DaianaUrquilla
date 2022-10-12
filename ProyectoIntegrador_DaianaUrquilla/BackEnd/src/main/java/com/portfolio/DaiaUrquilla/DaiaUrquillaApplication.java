85c9942a7058ff61d2b91ab208d4b4329e470171
Daia <94022914+DaiaUrq@users.noreply.github.com>
2022-10-12 19:44:18 -0300

    carpeta BackEnd

---
+++ ProyectoIntegrador_DaianaUrquilla/BackEnd/src/main/java/com/portfolio/DaiaUrquilla/DaiaUrquillaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.DaiaUrquilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaiaUrquillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaiaUrquillaApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_DaianaUrquilla/BackEnd/src/test/java/com/portfolio/DaiaUrquilla/DaiaUrquillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.DaiaUrquilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaiaUrquillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
